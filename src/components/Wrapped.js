d5268b4a65ac2a910b201de5fe5d35c138202230
Rong Feng Ye <63319433+Rongfengye@users.noreply.github.com>
2021-01-07 15:40:08 -0500

    prepare for pull request

--- src/components/Wrapped.js
+++ src/components/Wrapped.js
@@ -1,48 +1,55 @@
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 import "../wrapped.css";
-import joe from "../pictures/AverageJoe.jpg"
+import "../index.css"
+import joe from "../pictures/AverageJoe.jpg";
 
 export default class Wrapped extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="grid grid-cols-6 gap-4">
+      <div class="container">
+        <div class="grid grid-cols-6 gap-4">
           <div
-            className="col-start-1 col-span-3 ..."
-            style={{ backgroundColor: "red" , font: "Helvetica 20px bold italic"}}
+            class="col-start-1 col-span-3 ..."
+            // style={{
+            //   backgroundColor: "red",
+            //   font: "Helvetica 20px bold italic",
+            // }}
           >
             Hello John,
           </div>
           <div
-            className="col-start-1 col-span-3 ..."
-            style={{ backgroundColor: "red" , font: "Helvetica 20px bold italic"}}
+            class="col-start-1 col-span-3 ..."
+            // style={{
+            //   backgroundColor: "red",
+            //   font: "Helvetica 20px bold italic",
+            // }}
           >
             Your Yearly Finances, Wrapped Up:
           </div>
           <div
-            className="col-start-2 col-end-3 ..."
-            style={{ backgroundColor: "blue" }}
+            class="col-start-2 col-end-3 ..."
+            // style={{ backgroundColor: "blue" }}
           >
-            <img src={joe} alt="generic picture"/>
+            <img src={joe} alt="generic picture" />
           </div>
+
           <div
-            className="col-end-6 col-span-2 ..."
-            style={{ backgroundColor: "green" }}
+            class="col-end-6 col-span-2 ..."
           >
-            <h2 style={{textDecoration: "underline"}}>
+            <h2
+              class="text-red-500 font-sans"
+            >
               Top Sources of Income
             </h2>
 
-            <h2 style={{textDecoration: "underline"}}>
-              Largest Expenses
-            </h2>
+            {/* <h2 style={{ textDecoration: "underline" }}>Largest Expenses</h2> */}
           </div>
           <div
-            className="col-start-1 col-end-4 ..."
-            style={{ backgroundColor: "gray" }}
+            class="col-start-1 col-end-4 ..."
+            // style={{ backgroundColor: "gray" }}
           >
-            Budgeting Goal: 
+            Budgeting Goal:
           </div>
         </div>
       </div>
